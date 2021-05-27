374a27d12a4af4c9b9908ee0b2041812dcbe4163
theakashsingh <singhaksh@gmail.com>
2021-05-27 13:14:30 +0530

    project completed

---
+++ src/Sdata.js
@@ -0,0 +1,44 @@
+const Sdata = [
+    {
+      id:1,
+      imgsrc:"https://wallpaperaccess.com/full/1605487.jpg",
+      title:"Netflix Original Series",
+      sname:"Dark",
+      link:"https://www.netflix.com/in/title/80100172"
+    },
+
+    {
+        id:2,
+        imgsrc:"https://sunnyrathod.com/wp-content/uploads/2020/04/money-heist.jpg",
+        title:"Netflix Original Series",
+        sname:"Money Heist",
+        link:"https://www.netflix.com/in/title/80192098"
+      },
+
+      { 
+        id:3,
+        imgsrc:"https://m.media-amazon.com/images/M/MV5BMmMzMDI4NzMtZGMyYS00MjJhLWI0OWUtMTYxZDZlMjMzYjk0XkEyXkFqcGdeQXVyNjEwNTM2Mzc@._V1_UY268_CR4,0,182,268_AL_.jpg",
+        title:"Netflix Original Series",
+        sname:"Dark Desire",
+        link:"https://www.netflix.com/in/title/81090319"
+      },
+      
+      { id:4,
+        imgsrc:"https://m.media-amazon.com/images/M/MV5BMjI5Mjg1NDcyOV5BMl5BanBnXkFtZTgwMjAxOTQ5MTI@._V1_.jpg",
+        title:"Netflix Original Series",
+        sname:"Iron Fist",
+        link:"https://www.netflix.com/in/title/80002612"
+      },
+
+      {
+        id:5,
+        imgsrc:"https://static1.comicvine.com/uploads/original/6/67663/5449559-arrow.jpg",
+        title:"Netflix Original Series",
+        sname:"Arrow",
+        link:"https://www.netflix.com/in/title/70242081"
+      }
+
+
+]
+
+export default Sdata;
